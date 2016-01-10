0f92519fff28f7ad4527dcf85ac441915339a2fe
Alexander Khokhlov <alexander.khokhlov@gmail.com>
2016-01-10 19:10:26 +0300

    добавлены юазовые классы для PostgreSQL

---
+++ include/db/postgres.h
@@ -0,0 +1,51 @@
+﻿#ifndef __POSTGRES_H__
+#define __POSTGRES_H__
+
+#ifndef WIN32
+/* For sockaddr_in */
+#include <netinet/in.h>
+/* For socket functions */
+#include <sys/socket.h>
+#include <unistd.h>
+#else
+#include <winsock2.h>
+#endif  // Win32
+
+#include <string>
+#include <pqxx/pqxx>
+#include <boost/scoped_ptr.hpp>
+
+namespace DB {
+    class Postgres {
+        int         m_nPort;
+        std::string m_sHost;
+        std::string m_sDatabase;
+        std::string m_sLogin;
+        std::string m_sPassword;
+    protected:
+        boost::scoped_ptr<pqxx::connection> m_DBConnection;
+    public:
+        Postgres() {
+        }
+        virtual ~Postgres() {
+            Disconnect();
+        }
+
+        bool Reconnect();
+        bool Disconnect();
+
+        bool CheckConnect();
+
+        void    SetConnect(std::string sHost_, int nPort_, std::string sDatabase_, std::string sLogin_, std::string sPassword_) {
+            m_sHost = sHost_;
+            m_nPort = nPort_;
+            m_sDatabase = sDatabase_;
+            m_sLogin = sLogin_;
+            m_sPassword = sPassword_;
+
+            Reconnect();
+        }
+    };
+}
+
+#endif  // __POSTGRES_H__
---
+++ src/db/postgres.cpp
@@ -0,0 +1,53 @@
+﻿#include "db/postgres.h"
+
+#ifdef WIN32
+#ifdef _DEBUG
+#pragma comment(lib, "libpqxxD")
+#else
+#pragma comment(lib, "libpqxx")
+#endif
+#endif // WIN32
+
+using namespace DB;
+
+bool Postgres::Reconnect() {
+    std::string sc;
+    sc += " host = " + m_sHost;
+    sc += " port = " + std::to_string(m_nPort);
+    sc += " dbname = " + m_sDatabase;
+    sc += " user = " + m_sLogin;
+    sc += " password = " + m_sPassword;
+
+    m_DBConnection.reset(new pqxx::connection(sc));
+    return true;
+}
+
+bool Postgres::Disconnect() {
+    m_DBConnection.reset(NULL);
+    return true;
+}
+
+bool Postgres::CheckConnect() {
+    static int times = 0;
+    try {
+        times++;
+        if (!m_DBConnection->is_open()) {
+            m_DBConnection->activate();
+        }
+        times = 0;
+        // hack
+        pqxx::read_transaction txn(*m_DBConnection);
+
+        pqxx::result r = txn.exec(
+            "SELECT user_id "
+            " FROM users "
+            " LIMIT 1 ");
+    } catch (const pqxx::broken_connection &) {
+        if (times > 10) {
+            times = 0;
+            return false;
+        }
+        CheckConnect();
+    }
+    return true;
+}
