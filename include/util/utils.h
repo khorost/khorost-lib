7c2b167be7fda422902c1f175b44ac4ddbb698f4
Khokhlov Alexander <ak@khorost.net>
2019-01-15 17:58:40 +0300

    time zone

--- include/util/utils.h
+++ include/util/utils.h
@@ -3,6 +3,7 @@
 #include <string>
 
 #include <boost/date_time/posix_time/posix_time_types.hpp>
+#include <boost/date_time/local_time_adjustor.hpp>
 
 namespace khorost {
     namespace data {
@@ -12,5 +13,7 @@ namespace khorost {
         boost::posix_time::ptime epoch_milliseconds2ptime(uint64_t ms);
 
         std::string clear_html_tags(const std::string source);
+
+        typedef boost::date_time::local_adjustor<boost::posix_time::ptime, +3, boost::posix_time::no_dst> tz_msk;
     }
 }
