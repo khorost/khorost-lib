f2e880c8a976b2c407863c2513d7d54fe2dbebf4
Khokhlov Alexander <ak@khorost.net>
2019-03-08 23:29:27 +0300

    profiler

--- include/util/profiler.h
+++ include/util/profiler.h
@@ -10,13 +10,13 @@ namespace khorost {
             enum precision { seconds, milliseconds, microseconds, nanoseconds };
 
         private:
-            boost::posix_time::ptime m_start;
-            std::shared_ptr<spdlog::logger> m_logger;
-            std::string m_tag;
-            precision m_precision;
+            boost::posix_time::ptime m_start_;
+            std::shared_ptr<spdlog::logger> m_logger_;
+            std::string m_tag_;
+            precision m_precision_;
         public:
-            cpu(std::shared_ptr<spdlog::logger> logger, std::string tag, const precision log_precision = microseconds) : m_logger(std::move(logger)), m_tag(std::move(tag)), m_precision(log_precision) {
-                m_start = boost::posix_time::microsec_clock::universal_time();
+            cpu(std::shared_ptr<spdlog::logger> logger, std::string tag, const precision log_precision = microseconds) : m_logger_(std::move(logger)), m_tag_(std::move(tag)), m_precision_(log_precision) {
+                m_start_ = boost::posix_time::microsec_clock::universal_time();
             }
 
             ~cpu() {
@@ -24,20 +24,21 @@ namespace khorost {
             }
 
             void print() const {
-                if (m_logger != nullptr) {
-                    switch (m_precision) {
+                if (m_logger_ != nullptr) {
+                    const auto dt = boost::posix_time::microsec_clock::universal_time() - m_start_;
+                    switch (m_precision_) {
                     case nanoseconds:
-                        m_logger->debug("[PROFILER] {} = {} nanoseconds", m_tag, (boost::posix_time::microsec_clock::universal_time() - m_start).total_nanoseconds());
+                        m_logger_->debug("[PROFILER] {:>8} nanoseconds - {}", dt.total_nanoseconds(), m_tag_);
                         break;
                     case microseconds:
-                        m_logger->debug("[PROFILER] {} = {} microseconds", m_tag, (boost::posix_time::microsec_clock::universal_time() - m_start).total_microseconds());
+                        m_logger_->debug("[PROFILER] {:>8} microseconds - {}", dt.total_microseconds(), m_tag_);
                         break;
                     case milliseconds:
-                        m_logger->debug("[PROFILER] {} = {} milliseconds", m_tag, (boost::posix_time::microsec_clock::universal_time() - m_start).total_milliseconds());
+                        m_logger_->debug("[PROFILER] {:>8} milliseconds - {}", dt.total_milliseconds(), m_tag_);
                         break;
                     case seconds:
                     default:
-                        m_logger->debug("[PROFILER] {} = {} seconds", m_tag, (boost::posix_time::microsec_clock::universal_time() - m_start).total_seconds());
+                        m_logger_->debug("[PROFILER] {:>8} seconds - {}", dt.total_seconds(), m_tag_);
                         break;
                     }
                 }
@@ -51,6 +52,7 @@ namespace khorost {
 #define __PRETTY_FUNCTION__   __FUNCTION__
 #endif
 
+#define PROFILER_FUNCTION_TAG(LOGGER, TAG, ...)   khorost::profiler::cpu __cpu_profiler__(LOGGER, TAG + " " + __FUNCTION__ + std::string(" (" __FILE__ ":") + std::string(TOSTRING(__LINE__) ")"), ## __VA_ARGS__)
 #define PROFILER_FUNCTION(LOGGER, ...)   khorost::profiler::cpu __cpu_profiler__(LOGGER, __FUNCTION__ + std::string(" (" __FILE__ ":") + std::string(TOSTRING(__LINE__) ")"), ## __VA_ARGS__)
 #define PROFILER_TAG(LOGGER, TAG, ...)   khorost::profiler::cpu __cpu_profiler__##TAG(LOGGER, #TAG, ## __VA_ARGS__)
     }
